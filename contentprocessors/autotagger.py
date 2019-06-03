62a2a4dd1fc5c8d830ec6b53863c75a4884f8cfc
Siddhant Routray <siddhant.routray@aptusdatalabs.com>
2019-06-03 12:25:14 +0530

    Update autotagger.py

--- contentprocessors/autotagger.py
+++ contentprocessors/autotagger.py
@@ -134,20 +134,6 @@ class AutoTagger:
         self.logger.LogMessage('verbose', 'fileContent --------------------- {0}'.format(fileContent))
 
 
-        nlp = spacy.load("en_core_web_sm")
-        person = 0    
-        for sentence in text.split("."):
-            
-            doc = nlp(sentence)
-            ents = [(e.text,e.label_) for e in doc.ents]
-            for i in ents:
-                if i[1] == 'PERSON':
-                        person = 1
-                        print(i[0])
-                        break
-            
-        if(person==1):
-            self.AddTagToAmbarFile(AmbarFile['file_id'], AmbarFile['meta']['full_name'], self.AUTO_TAG_TYPE, 'person')
-            person = 0
+        
 
 
